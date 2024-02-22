e2b220071b18762b6aca5d02b280dd13e9426b82
Saba Marghania <marghanias@gmail.com>
2024-02-23 02:00:13 +0400

    project initialize

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import logo from "../images/logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-container">
+      <img className="logo" src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
